f3e2eedcb7f5afae298402efda8107370b323cef
Lin Tan <tan.esther8@gmail.com>
2020-12-11 21:18:06 -0800

    Updated server

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ mongoose.connect("mongodb://localhost:27017/forumDB", { useUnifiedTopology: true
 //schema
 const passwordSchema = new mongoose.Schema({
   firstname = String,
-  lastname = String
+  lastname = String,
   username: String,
-  pass: String,
+  pass: String
 });
 
 const Password = mongoose.model("Password", passwordSchema);
