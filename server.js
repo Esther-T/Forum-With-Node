48231c777d6886e4eeffc1776bf8515796e4ab7d
Lin Tan <tan.esther8@gmail.com>
2020-12-31 20:27:05 -0800

    Worked on hashing passwords

--- server.js
+++ server.js
@@ -174,14 +174,14 @@ app.post("/post-comment", function(req, res){
 
 //generate the salt for pass
 let generateSalt = rounds => {
-    if (rounds >= 15) {
+    if (rounds >= 5) {
         throw new Error(`${rounds} is greater than 15,Must be less that 15`);
     }
     if (typeof rounds !== 'number') {
         throw new Error('rounds param must be a number');
     }
     if (rounds == null) {
-        rounds = 12;
+        rounds = 5;
     }
     return crypto.randomBytes(Math.ceil(rounds / 2)).toString('hex').slice(0, rounds);
 };
